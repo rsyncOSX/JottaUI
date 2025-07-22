0cbb25a1bbe4fb4bd17489e1d8b3e2795b48a34c
Thomas Evensen <thomeven@gmail.com>
2025-07-22 15:27:52 +0200

    Add command picker for help tasks in status view
    Introduced a TypeofCommands enum and a Picker UI to select help commands in JottaStatusOutputView. The help() function now executes the selected command's help task, improving user interaction and command selection.

--- JottaUI/Views/TextViews/JottaStatusOutputView.swift
+++ JottaUI/Views/TextViews/JottaStatusOutputView.swift
@@ -7,11 +7,53 @@
 
 import SwiftUI
 
+enum TypeofCommands: String, CaseIterable, Identifiable, CustomStringConvertible {
+    case select
+    
+    case add
+    case archive
+    case completion
+    case config
+    
+    case download
+    case dump
+    case help
+    case ignores
+    
+    case list
+    case logfile
+    case login
+    case logout
+    
+    case ls
+    case observe
+    case pause
+    case rem
+    
+    case resume
+    case scan
+    case share
+    case status
+    
+    case sync
+    case tail
+    case trash
+    case version
+    
+    case web
+    case webhook
+    
+
+    var id: String { rawValue }
+    var description: String { rawValue.localizedLowercase }
+}
+
 struct JottaStatusOutputView: View {
     @Binding var completedjottastatustextview: Bool
 
     @State private var showprogressview = false
     @State private var jottaclioutput = ObservableJottaOutput()
+    @State private var selectedhelpcommand = TypeofCommands.select
 
     var body: some View {
         NavigationStack {
@@ -27,14 +69,12 @@ struct JottaStatusOutputView: View {
                         }
                         .buttonStyle(ColorfulButtonStyle())
                         
-                        Button {
-                            list()
-                        } label: {
-                            Text("List")
+                        pickerselecttypeofhelptask
+                            .onChange(of: selectedhelpcommand) {
+                                guard selectedhelpcommand != .select else { return }
+                                help()
+                            }
                         }
-                        .buttonStyle(ColorfulButtonStyle())
-                    }
-                    
                 }
             }
             .padding()
@@ -61,9 +101,34 @@ struct JottaStatusOutputView: View {
                 abort()
             })
     }
+    
+    var pickerselecttypeofhelptask: some View {
+        Picker(NSLocalizedString("Help on", comment: "") + ":",
+               selection: $selectedhelpcommand)
+        {
+            ForEach(TypeofCommands.allCases) { Text($0.description)
+                .tag($0)
+            }
+        }
+        .pickerStyle(DefaultPickerStyle())
+        .frame(width: 160)
+    }
 }
 
 extension JottaStatusOutputView {
+    
+    func help() {
+        let arguments = [selectedhelpcommand.rawValue, "--help"]
+        let command = FullpathJottaCli().jottaclipathandcommand()
+
+        // Start progressview
+        showprogressview = true
+        let process = ProcessCommand(command: command,
+                                     arguments: arguments,
+                                     processtermination: processtermination)
+        process.executeProcess()
+    }
+    
     func scan() {
         let arguments = ["status"]
         let command = FullpathJottaCli().jottaclipathandcommand()
