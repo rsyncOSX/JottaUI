ddf44d417aa79dd64f244afa14629c68bb859049
Thomas Evensen <thomeven@gmail.com>
2025-08-27 08:25:07 +0200

    Update JottaStatusOutputView.swift

--- JottaUI/Views/StatusView/JottaStatusOutputView.swift
+++ JottaUI/Views/StatusView/JottaStatusOutputView.swift
@@ -13,68 +13,52 @@ struct JottaStatusOutputView: View {
     var body: some View {
         Table(output) {
             TableColumn("Status Jotta-cli" + ": \(output.count) rows") { data in
-                
                 var seconds: Double {
                     if var date = extractDate(from: data.record), let year = Date().year() {
                         date = String(year) + " " + date
                         if let lastbackup = date.status_date_from_string() {
                             return lastbackup.timeIntervalSinceNow * -1
                         }
-                        
+
                     } else {
                         return 0
                     }
                     return 0
                 }
-                
-                
+
                 if data.record.contains("------") {
                     Text("")
                 } else {
                     if data.record.contains("Up to date") {
-                        
                         if seconds > 0 {
-                            
                             let minutessince = String(format: "%.2f", seconds / 60)
-                            
+
                             Text(data.record) + Text(" (\(minutessince) min ago)")
                                 .foregroundColor(Color.green)
-                            
+
                         } else {
-                            
                             Text(data.record)
-                                
                         }
-                        
-                        
+
                     } else if data.record.contains("Transferring") {
-                        
                         if seconds > 0 {
-                            
                             let minutessince = String(format: "%.2f", seconds / 60)
-                            
+
                             Text(data.record) + Text(" (\(minutessince) min ago)")
                                 .foregroundColor(Color.red)
-                            
+
                         } else {
-                            
                             Text(data.record)
-                                
                         }
-                        
-                        
-                        
+
                     } else {
                         if seconds > 0 {
-                            
                             let minutessince = String(format: "%.2f", seconds / 60)
-                            
+
                             Text(data.record) + Text(" (\(minutessince) min ago)")
-                            
+
                         } else {
-                            
                             Text(data.record)
-                                
                         }
                     }
                 }
